2eef2922b36435cbf2fc1a236d2340c11bb69104
Ocean-code-1995 <56520457+Ocean-code-1995@users.noreply.github.com>
2024-02-16 15:36:21 +0100

    added dependencies to req.txt and dlt pipeline for all required data for hw4

--- Week4/dlt_project/taxi_data_pipeline.py
+++ Week4/dlt_project/taxi_data_pipeline.py
@@ -1,50 +1,65 @@
 import dlt
+import numpy as np
+import pandas as pd
 from dlt.sources.helpers import requests
+import time
 
 
-@dlt.source
-def taxi_data_pipeline_source(api_secret_key=dlt.secrets.value):
-    return taxi_data_pipeline_resource(api_secret_key)
-
-
-def _create_auth_headers(api_secret_key):
-    """Constructs Bearer type authorization header which is the most common authorization method"""
-    headers = {"Authorization": f"Bearer {api_secret_key}"}
-    return headers
-
-
-@dlt.resource(write_disposition="append")
-def taxi_data_pipeline_resource(api_secret_key=dlt.secrets.value):
-    headers = _create_auth_headers(api_secret_key)
-
-    # check if authentication headers look fine
-    print(headers)
-
-    # make an api call here
-    # response = requests.get(url, headers=headers, params=params)
-    # response.raise_for_status()
-    # yield response.json()
-
-    # test data for loading validation, delete it once you yield actual data
-    test_data = [{"id": 0}, {"id": 1}]
-    yield test_data
+@dlt.resource(write_disposition='replace')
+def fetch_data_resource(trip_obj):
+    """
+    Fetch data from the source
+    :param trip_obj: dict containing the type of trip as string and the years to fetch as a list
+    :return: response containing the fetched data
+    """
+    trip_type = trip_obj['type']
+    for year in trip_obj['years']:
+        print(f"Fetching data for year {year}")
+        for month in range(1, 13):
+            url = f'https://d37ci6vzurychx.cloudfront.net/trip-data/{trip_type}_tripdata_{year}-{month:02d}.parquet'
+            print(f"Downloading {trip_type} trip data for {year}-{month:02d}")
+            response = requests.get(url)
+            response.raise_for_status()
+            df = pd.read_parquet(url, engine='pyarrow')
+            # in case of yellow taxi and fhv trip data, extra columns need to handled
+            if trip_type == 'yellow':
+                if 'airport_fee' in df.columns:
+                    df['airport_fee'] = df['airport_fee'].astype(float)
+                else:
+                    df['airport_fee'] = np.nan
+            if trip_type == 'fhv':
+                if 'p_ulocation_id' in df.columns:
+                    df['PUlocationID'] = df['PUlocationID'].astype(float)
+                else:
+                    df['PUlocationID'] = np.nan
+                if 'd_olocation_id' in df.columns:
+                    df['DOlocationID'] = df['DOlocationID'].astype(float)
+                else:
+                    df['DOlocationID'] = np.nan
+            yield df
+        print(f" -> Downloaded {trip_type} trip data for {year}\n")
+    print(f" ---> Downloaded {trip_type} trip data for all years")
 
 
 if __name__ == "__main__":
+    # define the needed data
+    needed_data = [
+        {'type': 'green', 'years': [2019, 2020]},
+        {'type': 'yellow', 'years': [2019, 2020]},
+        {'type': 'fhv', 'years': [2019]}
+    ]
     # configure the pipeline with your destination details
-    pipeline = dlt.pipeline(
-        pipeline_name='taxi_data_pipeline', destination='filesystem', dataset_name='taxi_data_pipeline_data'
-    )
-
-    # print credentials by running the resource
-    data = list(taxi_data_pipeline_resource())
-
-    # print the data yielded from resource
-    print(data)
-    exit()
-
-    # run the pipeline with your parameters
-    load_info = pipeline.run(taxi_data_pipeline_source())
-
-    # pretty print the information on data that was loaded
-    print(load_info)
+    start = time.time()
+    for collection in needed_data:
+        pipeline = dlt.pipeline(
+            pipeline_name=f'tlc_{collection["type"]}_data',
+            destination='filesystem',
+            dataset_name=f'tlc_{collection["type"]}_data'
+        )
+        print(f'Running pipeline for {collection["type"]} trip data')
+        run = pipeline.run(data=fetch_data_resource(collection))
+        print(run)
+        print(f'Pipeline for {collection["type"]} trip data completed')
+        print('--')
+    end = time.time()
+    print('All done! in' + str(end - start) + ' seconds')
