bd89f1e867a0c9fbb3a44ff56dc7751618d8a26d
s.wefers@student.xu-university.de <s.wefers@student.xu-university.de>
2024-02-20 14:06:42 +0000

    successfully build fact table

--- Week4/dbt_project/models/staging/stg_green_tripdata.sql
+++ Week4/dbt_project/models/staging/stg_green_tripdata.sql
@@ -4,29 +4,29 @@
     )
 }}
 
-with tripdata as 
+with tripdata as
 (
   select *,
-    row_number() over(partition by vendor_id, lpep_pickup_datetime) as rn
-  from {{ source('staging','green_taxi') }}
-  where vendor_id is not null 
+  row_number() over(partition by vendor_id, lpep_pickup_datetime) as rn
+  from {{ source('staging', 'green_taxi')}}
+  where vendor_id is not null
 )
-
 select
-    {{ dbt_utils.generate_surrogate_key(['vendor_id', 'lpep_pickup_datetime']) }} as trip_id,
+
     -- identifiers
+    {{ dbt_utils.generate_surrogate_key(['vendor_id', 'lpep_pickup_datetime']) }} as trip_id,
     cast(vendor_id as integer) as vendor_id,
     cast(ratecode_id as integer) as ratecode_id,
-    cast(pu_location_id as integer) as pu_location_id,
-    cast(do_location_id as integer) as do_location_id,
-    
+    cast(pu_location_id as integer) as pickup_location_id,
+    cast(do_location_id as integer) as dropoff_location_id,
+
     -- timestamps
     cast(lpep_pickup_datetime as timestamp) as pickup_datetime,
     cast(lpep_dropoff_datetime as timestamp) as dropoff_datetime,
-    
+
     -- trip info
     store_and_fwd_flag,
-    cast(passenger_count as numeric) as passenger_count,
+    cast(passenger_count as integer) as passenger_count,
     cast(trip_distance as numeric) as trip_distance,
     cast(trip_type as integer) as trip_type,
 
@@ -38,17 +38,14 @@ select
     cast(tolls_amount as numeric) as tolls_amount,
     cast(ehail_fee as numeric) as ehail_fee,
     cast(improvement_surcharge as numeric) as improvement_surcharge,
-    cast(total_amount as numeric) as total_amount,
     cast(payment_type as integer) as payment_type,
-    cast(congestion_surcharge as numeric) as congestion_surcharge,
-
-    -- create new column
+    cast(total_amount as numeric) as total_amount,
     {{ get_payment_type_description('payment_type') }} as payment_type_description
+
 from tripdata
 where rn = 1
 
-
--- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
+-- dbt build --select <model_name> --vars '{ is_test_run: false }'
 {% if var('is_test_run', default=true) %}
 
   limit 100
